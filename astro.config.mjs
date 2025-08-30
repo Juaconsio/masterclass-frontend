b2b54bd0806cc0d4bd432c3d736690320370e297
juaconsio <jconchan@outlook.com>
2025-08-30 14:48:09 -0400

    add config: prettier, astro, ts, tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      {
+        name: "@astrojs/ts-plugin",
+      },
+    ],
   },
 
   adapter: vercel(),
+  integrations: [react()],
 });
