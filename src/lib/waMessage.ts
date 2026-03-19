4f2670aa9b298ec307247e881b1b6df9802b4574
Joaquín Concha <53885988+Juaconsio@users.noreply.github.com>
2026-03-19 12:56:33 -0300

    Update waMessage.ts

--- src/lib/waMessage.ts
+++ src/lib/waMessage.ts
@@ -1,3 +1,3 @@
-const waMessage = 'Hola, revise el sitio de SalvaRamos y quisieramos saber más.';
-const waUrl = `https://wa.me/56934476377?text=${encodeURIComponent(waMessage)}`;
+const waMessage = 'Hola! Estuve viendo SalvaRamos y me gustaría saber más';
+const waUrl = `https://wa.me/56966749584?text=${encodeURIComponent(waMessage)}`;
 export default waUrl;
